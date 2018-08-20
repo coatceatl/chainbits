faf1c979d8c9ce9b530a188d574dd3b7330880a6
Oksana Tkachenko <oksana@crystax.net>
2018-08-21 01:00:48 +0300

    installed jqueru countdown

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import $ from 'jquery';
-
 //import 'bootstrap/dist/css/bootstrap.min.css';
-console.log('webpack 4');
+import 'jquery-countdown';
+
+$(document).ready(function() {
+  $("#getting-started").countdown("2018/09/01", function(event) {
+    $(this).text(event.strftime('%D days %H:%M:%S'));
+  });
+});
+
+
